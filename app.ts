d2347f20f0c1e4d90bd6abaa6f977e72472110af
Jan Dranreb Balangue <jandranrebbalangue@gmail.com>
2023-12-28 21:36:14 +0800

    Remove unused fastify-env and fix db env

--- app.ts
+++ app.ts
@@ -1,53 +1,22 @@
-import { TypeBoxTypeProvider } from "@fastify/type-provider-typebox"
 import fastify from "fastify"
 import http from "http"
-import { fastifyEnv } from "@fastify/env"
 import { registerRoutes } from "./routes"
 
-interface customRequest extends http.IncomingMessage { }
-const app = fastify<http.Server, customRequest>({
-  logger: true
-}).withTypeProvider<TypeBoxTypeProvider>()
-
-const schema = {
-  type: "object",
-  required: [
-    "DATABASE_URL",
-    "POSTGRES_USER",
-    "POSTGRES_HOST",
-    "POSTGRES_PASSWORD",
-    "POSTGRES_DB",
-    "POSTGRES_PORT"
-  ],
-  properties: {
-    DATABASE_URL: {
-      type: "string"
-    },
-    POSTGRES_USER: {
-      type: "string"
-    },
-    POSTGRES_HOST: {
-      type: "string"
-    },
-    POSTGRES_PASSWORD: {
-      type: "string"
-    },
-    POSTGRES_DB: {
-      type: "string"
-    },
-    POSTGRES_PORT: {
-      type: "number",
-      default: 5432
+declare module "fastify" {
+  interface FastifyInstance {
+    config: {
+      DATABASE_URL: string
+      POSTGRES_USER: string
+      POSTGRES_HOST: string
+      POSTGRES_PASSWORD: string
+      POSTGRES_DB: string
+      POSTGRES_PORT: number
     }
   }
 }
-const options = {
-  confKey: "config",
-  schema,
-  dotenv: true
-}
-app.register(fastifyEnv, options).ready((err) => {
-  if (err) console.error(err)
+
+const app = fastify<http.Server, http.IncomingMessage>({
+  logger: true
 })
 
 app.register(registerRoutes)
--- db/index.ts
+++ db/index.ts
@@ -1,15 +1,18 @@
 import { Pool } from "pg"
 import { Kysely, PostgresDialect } from "kysely"
+import dotenv from "dotenv"
 import { Database } from "./types"
+
+dotenv.config()
 
 const dialect = new PostgresDialect({
   pool: new Pool({
-    database: process.env.POSTGRES_DB ?? "postgres",
-    host: process.env.POSTGRES_HOST ?? "localhost",
-    user: process.env.POSTGRES_USER ?? "postgres",
+    database: process.env.POSTGRES_DB ?? "",
+    host: process.env.POSTGRES_HOST ?? "",
+    user: process.env.POSTGRES_USER ?? "",
     port: parseInt(process.env.POSTGRES_PORT as string, 10) ?? 5432,
     max: 10,
-    password: process.env.POSTGRES_PASSWORD ?? "root"
+    password: process.env.POSTGRES_PASSWORD ?? ""
   })
 })
 
